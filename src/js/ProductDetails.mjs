587d16def567d3911ac4d661a65c6727fce84f3a
Logan Stucki <loganstucki@gmail.com>
2023-11-08 16:51:15 -0600

    team activity 2

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,47 @@
+import { setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+
+  init() {
+    // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+    // once we have the product details we can render out the HTML
+    // once the HTML is rendered we can add a listener to Add to Cart button
+    // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
+    this.dataSource.findProductById(this.productId).then((product) => {
+      this.product = product;
+      this.renderProductDetails();
+
+      document
+        .getElementById("addToCart")
+        .addEventListener("click", this.addToCart.bind(this));
+    });
+  }
+
+  addToCart() {
+    setLocalStorage("so-cart", this.product);
+  }
+
+  renderProductDetails() {
+    let product = `
+    <h3>${this.product.Brand.Name}</h3>
+    <h2 class="divider">${this.product.NameWithoutBrand}</h2>
+    <img
+      class="divider"
+      src="${this.product.Image}"
+      alt="${this.product.NameWithoutBrand}"
+    />
+    <p class="product-card__price">$${this.product.FinalPrice}</p>
+    <p class="product__color">${this.product.Colors[0].ColorName}</p>
+    <p class="product__description">${this.product.DescriptionHtmlSimple}</p>
+    <div class="product-detail__add">
+      <button id="addToCart" data-id="344YJ">Add to Cart</button>
+    </div>`;
+
+    document.getElementsByClassName("product-detail")[0].innerHTML = product;
+  }
+}
